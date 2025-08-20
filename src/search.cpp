c75410be1542ccbbeee41644b9d5e43eb0286854
johndoknjas <jdoknjas@sfu.ca>
2025-08-20 11:42:18 -0700

    Do not prune at shallow depth in atomic when the kings are close to each other.

--- src/search.cpp
+++ src/search.cpp
@@ -1140,7 +1140,8 @@ moves_loop: // When in check, search starts from here
       // Step 13. Pruning at shallow depth (~200 Elo)
       if (  !rootNode
           && (pos.non_pawn_material(us) || pos.count<ALL_PIECES>(us) == pos.count<PAWN>(us))
-          && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
+          && bestValue > VALUE_TB_LOSS_IN_MAX_PLY
+          && (!pos.blast_on_capture() || distance(pos.square<COMMONER>(WHITE), pos.square<COMMONER>(BLACK)) > 2))
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
           moveCountPruning = moveCount >= futility_move_count(improving, depth, pos);
